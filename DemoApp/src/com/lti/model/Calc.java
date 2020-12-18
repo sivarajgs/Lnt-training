5f120967f585fb0b25ce4ca76a2c682c46f3c1f1
sivarajgs <76148814+sivarajgs@users.noreply.github.com>
2020-12-18 13:05:27 +0530

    added subs method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -7,5 +7,8 @@ public void add(int num1,int num2){
 public void subs(int num1,int num2){
 	System.out.println("diff="+(num1-num2));
 }
+public void div(int num1,int num2){
+	System.out.println("div="+(num1/num2));
+}
 
 }
