532661e739c1c867a5cdfcb9b220769578718244
sivarajgs <76148814+sivarajgs@users.noreply.github.com>
2020-12-18 15:54:35 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,5 +4,7 @@ public class Calc {
 public void add(int num1,int num2){
 	System.out.println("Sum="+(num1+num2));
 }
-
+public void sub(int num1,int num2){
+	System.out.println("Sum="+(num1-num2));
+}
 }
