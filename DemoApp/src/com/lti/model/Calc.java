80758067a8587ffbe439c3f38d0ba6cbb172a810
sivarajgs <sivarajfairlands@gmail.com>
2020-12-18 15:35:29 +0530

    removed method

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,15 +4,5 @@ public class Calc {
 public void add(int num1,int num2){
 	System.out.println("Sum="+(num1+num2));
 }
-public void subs(int num1,int num2){
-	System.out.println("diff="+(num1-num2));
-}
-public void div(int num1,int num2){
-	System.out.println("div="+(num1/num2));
-}
-
-public void mul(int num1,int num2){
-	System.out.println("mul="+(num1*num2));
-}
 
 }
